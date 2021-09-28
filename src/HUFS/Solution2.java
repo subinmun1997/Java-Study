9548cf06f58eda855e351936d6c4414816479140
subinmun1997 <qzxy812@gmail.com>
2021-09-28 22:14:10 +0900

    solution2

---
+++ src/HUFS/Solution2.java
@@ -0,0 +1,14 @@
+package HUFS;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        double radius;
+        double area;
+
+        radius = 20;
+
+        area = radius * radius * 3.14159;
+
+        System.out.println("The area for the circle of radius " + radius + " is " + area);
+    }
+}
