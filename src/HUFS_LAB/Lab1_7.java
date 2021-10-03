bae7aed7a73623e1c45d3af604e21d18e9ee6b12
subinmun1997 <qzxy812@gmail.com>
2021-10-03 21:59:48 +0900

    LAB 1.7

---
+++ src/HUFS_LAB/Lab1_7.java
@@ -0,0 +1,11 @@
+package HUFS_LAB;
+
+public class Lab1_7 {
+    public static void main(String[] args) {
+        double value1 = 4 * (1-1/3+1/5-1/7+1/9-1/11);
+        double value2 = 4 * (1-1/3+1/5-1/7+1/9-1/11+1/13);
+
+        System.out.println("The value of value1 is " + value1);
+        System.out.println("The value of value2 is " + value2);
+    }
+}
