e224585800514aee9935d7383c294089e0fd72da
subinmun1997 <qzxy812@gmail.com>
2021-10-03 21:55:34 +0900

    LAB 1.5

---
+++ src/HUFS_LAB/Lab1_5.java
@@ -0,0 +1,8 @@
+package HUFS_LAB;
+
+public class Lab1_5 {
+    public static void main(String[] args) {
+        double value = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5);
+        System.out.println("The result of value is " + value);
+    }
+}
