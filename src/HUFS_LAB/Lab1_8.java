5ef1b41ef492ad784df330291c4f417beb6b60a6
subinmun1997 <qzxy812@gmail.com>
2021-10-03 22:13:43 +0900

    LAB 1.8

---
+++ src/HUFS_LAB/Lab1_8.java
@@ -0,0 +1,13 @@
+package HUFS_LAB;
+
+public class Lab1_8 {
+    public static void main(String[] args) {
+        final double PI = 3.141592;
+        double radius = 5.5;
+        double perimeter = 2 * radius * PI;
+        double area = radius * radius * PI;
+
+        System.out.println("perimeter is " + perimeter);
+        System.out.println("area is " + area);
+    }
+}
