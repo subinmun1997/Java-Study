aace7c1897ca89ef4ccf12b03e0fa52165fa355b
subinmun1997 <qzxy812@gmail.com>
2021-10-04 17:36:51 +0900

    LAB 3.8

---
+++ src/HUFS_LAB/Lab3_5.java
@@ -0,0 +1,29 @@
+package HUFS_LAB;
+import java.util.Scanner;
+
+public class Lab3_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter three integers: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if (a>b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        if (b>c) {
+            int temp = b;
+            b = c;
+            c = temp;
+        }
+        if (a>b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        System.out.println("non-decreasing : " + a + b + c);
+    }
+}
