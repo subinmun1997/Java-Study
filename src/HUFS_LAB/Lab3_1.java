79d0bfbf2380be79ceecb56da90d42897870fda5
subinmun1997 <qzxy812@gmail.com>
2021-10-04 16:54:38 +0900

    LAB 3.1

--- src/HUFS_LAB/Lab3_1.java
+++ src/HUFS_LAB/Lab3_1.java
@@ -12,11 +12,11 @@ public class Lab3_1 {
         double crim = Math.pow(b,2) - 4*a*c;
 
         if (crim > 0) {
-            double r1 = ((-1) * b + Math.pow(b*b-4*a*c, 0.5)) / 2*a;
-            double r2 = ((-1) * b - Math.pow(b*b-4*a*c, 0.5)) / 2*a;
+            double r1 = ((-1) * b + Math.pow(crim, 0.5)) / 2*a;
+            double r2 = ((-1) * b - Math.pow(crim, 0.5)) / 2*a;
             System.out.println("The equation has two roots " + r1 + "and" + r2);
         } else if (crim == 0) {
-            double r1 = ((-1) * b + Math.pow(b*b-4*a*c, 0.5)) / 2*a;
+            double r1 = ((-1) * b + Math.pow(crim, 0.5)) / 2*a;
             System.out.println("The equation has one root " + r1 );
         } else {
             System.out.println("The equation has no real roots");
