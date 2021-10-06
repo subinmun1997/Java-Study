9582e1addc316f3d14e60d6987bf5b848b756770
subinmun1997 <qzxy812@gmail.com>
2021-10-06 12:24:46 +0900

    LAB 4.16

---
+++ src/HUFS_LAB/Lab4_16.java
@@ -0,0 +1,8 @@
+package HUFS_LAB;
+
+public class Lab4_16 {
+    public static void main(String[] args) {
+        int num = 65 + (int)(Math.random() * 26);
+        System.out.println((char)num);
+    }
+}
