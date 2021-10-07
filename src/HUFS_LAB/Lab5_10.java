ce125890a2de052a9006362b332db5c9e2c7ae30
subinmun1997 <qzxy812@gmail.com>
2021-10-07 16:21:30 +0900

    LAB 5.10

--- src/HUFS_LAB/Lab5_10.java
+++ src/HUFS_LAB/Lab5_10.java
@@ -2,15 +2,15 @@ package HUFS_LAB;
 
 public class Lab5_10 {
     public static void main(String[] args) {
-        int count = 1;
+        int count = 0;
         for(int i=100;i<=1000;i++) {
             if (i%5==0 && i%6==0) {
+                ++count;
                 System.out.print(i + " ");
-                count++;
             }
             if(count==10) {
                 System.out.println();
-                count = 1;
+                count = 0;
             }
         }
     }
