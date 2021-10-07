f0551622d09c03a7d4e94c2592669cbcfd7e6e44
subinmun1997 <qzxy812@gmail.com>
2021-10-07 16:27:03 +0900

    LAB 5.12

--- src/HUFS_LAB/Lab5_12.java
+++ src/HUFS_LAB/Lab5_12.java
@@ -1,4 +1,13 @@
 package HUFS_LAB;
 
 public class Lab5_12 {
+    public static void main(String[] args) {
+        int n = 1;
+        while(true) {
+            if(Math.pow(n,2) > 12000)
+                break;
+            n++;
+        }
+        System.out.println("n is " + n);
+    }
 }
