4b5408b9ea28fb1d5a2af810b44f9cf71d2361b0
subinmun1997 <qzxy812@gmail.com>
2021-10-07 12:41:41 +0900

    Solution11

---
+++ src/HUFS/Solution11.java
@@ -0,0 +1,10 @@
+package HUFS;
+
+public class Solution11 {
+    public static void main(String[] args) {
+        int i;
+        for(i=0;i<100;i++) {
+            System.out.println("Welcome to Java!");
+        }
+    }
+}
