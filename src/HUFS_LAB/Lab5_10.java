a48012a6243c2a2d838e9e84256e6633bb68c11a
subinmun1997 <qzxy812@gmail.com>
2021-10-07 16:19:46 +0900

    LAB 5.10

--- src/HUFS_LAB/Lab5_10.java
+++ src/HUFS_LAB/Lab5_10.java
@@ -1,4 +1,17 @@
 package HUFS_LAB;
 
 public class Lab5_10 {
+    public static void main(String[] args) {
+        int count = 1;
+        for(int i=100;i<=1000;i++) {
+            if (i%5==0 && i%6==0) {
+                System.out.print(i + " ");
+                count++;
+            }
+            if(count==10) {
+                System.out.println();
+                count = 1;
+            }
+        }
+    }
 }
