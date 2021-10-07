ab16fb09bff8c119dfb824574648d11c2652a77a
subinmun1997 <qzxy812@gmail.com>
2021-10-07 12:34:54 +0900

    Solution10

---
+++ src/HUFS/Solution10.java
@@ -0,0 +1,11 @@
+package HUFS;
+
+public class Solution10 {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 100) {
+            System.out.println("Welcome to Java");
+            count++;
+        }
+    }
+}
