d78b75d2ae5ca3be7b7e7e534e37c0dda3ecb325
subinmun1997 <qzxy812@gmail.com>
2021-10-07 17:36:56 +0900

    LAB 5.14

--- src/HUFS_LAB/Lab5_14.java
+++ src/HUFS_LAB/Lab5_14.java
@@ -1,4 +1,19 @@
 package HUFS_LAB;
+import java.util.Scanner;
 
 public class Lab5_14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter two positive integers: ");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        int d = (num1 < num2) ? num1 : num2;
+        for (; d >= 1; d--) {
+            if ((num1 % d == 0) && (num2 % d == 0)) {
+                break;
+            }
+        }
+        System.out.println("gcd is " + d);
+    }
 }
