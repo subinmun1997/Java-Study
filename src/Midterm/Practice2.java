eb0a06c4504eb0635dd11594826d89a11ef154df
subinmun1997 <qzxy812@gmail.com>
2021-10-10 16:48:11 +0900

    Practice 2

---
+++ src/Midterm/Practice2.java
@@ -0,0 +1,11 @@
+package Midterm;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        double v1 = 4 * (1 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11);
+        double v2 = 4 * (1 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11 + 1.0/13);
+
+        System.out.println("The result of v1 is " + v1);
+        System.out.println("The result of v2 is " + v2);
+    }
+}
