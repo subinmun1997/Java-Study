d2db8f278e7508a6cb299214ccc2532c93da355a
subinmun1997 <qzxy812@gmail.com>
2021-10-10 16:58:46 +0900

    Practice 4

---
+++ src/Midterm/Practice4.java
@@ -0,0 +1,12 @@
+package Midterm;
+
+public class Practice4 {
+    public static void main(String[] args) {
+        double denom = 3.4 * 0.55 - 50.2 * 2.1;
+        double x = 44.5 * 0.55 - 50.2 * 5.9 / denom;
+        double y = 3.4 * 5.9 - 44.5 * 2.1 / denom;
+
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+    }
+}
