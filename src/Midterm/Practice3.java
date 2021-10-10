79a346dc3984d8a2f60ba0f4c4985c248b4183ec
subinmun1997 <qzxy812@gmail.com>
2021-10-10 16:54:47 +0900

    Practice 3

---
+++ src/Midterm/Practice3.java
@@ -0,0 +1,12 @@
+package Midterm;
+
+public class Practice3 {
+    public static void main(String[] args) {
+        double radius = 5.5;
+        double perimeter = 2 * radius * Math.PI;
+        double area = radius * radius * Math.PI;
+
+        System.out.println("The area of a circle is " + area);
+        System.out.println("The perimeter of a circle is " + perimeter);
+    }
+}
