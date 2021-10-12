1e8670a74145079b7f9e6e4319f7cb82053ef2ce
subinmun1997 <qzxy812@gmail.com>
2021-10-12 16:00:54 +0900

    Practice 25

---
+++ src/Midterm/Practice25.java
@@ -0,0 +1,8 @@
+package Midterm;
+
+public class Practice25 {
+    public static void main(String[] args) {
+        int num = 65 + (int)(Math.random() * 26);
+        System.out.println((char)num);
+    }
+}
