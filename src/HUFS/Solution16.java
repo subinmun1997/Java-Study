5e354f0cb5d0a9621f784e22f72ef7a789e80403
subinmun1997 <qzxy812@gmail.com>
2021-10-17 16:19:36 +0900

    Solution16

--- src/HUFS/Solution16.java
+++ src/HUFS/Solution16.java
@@ -1,4 +1,23 @@
 package HUFS;
 
+import java.util.Scanner;
+
 public class Solution16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter x, y: ");
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        int z = max(x,y);
+        System.out.println("Max is " + z);
+    }
+    public static int max(int num1, int num2) {
+        int result;
+        if(num1>num2)
+            result = num1;
+        else
+            result = num2;
+        return result;
+    }
 }
