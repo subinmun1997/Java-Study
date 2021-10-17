2de8d59e232414057ed573f7085b8cc4047bd3cb
subinmun1997 <qzxy812@gmail.com>
2021-10-17 16:35:22 +0900

    Solution18

---
+++ src/HUFS/Solution18.java
@@ -0,0 +1,13 @@
+package HUFS;
+
+public class Solution18 {
+    public static void main(String[] args) {
+        nPrintln("Welcome to Java", 5);
+        nPrintln("Computer Science", 15);
+
+    }
+    public static void nPrintln(String message, int n) {
+        for(int i=0;i<n;i++)
+            System.out.println(message);
+    }
+}
