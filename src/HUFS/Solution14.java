44f94b638b4d78a16f30e9eab037b73298430a5d
subinmun1997 <qzxy812@gmail.com>
2021-10-17 16:12:59 +0900

    Solution14

---
+++ src/HUFS/Solution14.java
@@ -0,0 +1,20 @@
+package HUFS;
+
+public class Solution14 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i=1;i<=10;i++)
+            sum += i;
+        System.out.println("Sum from 1 to 10 is " + sum);
+
+        sum = 0;
+        for(int i=20;i<=30;i++)
+            sum += i;
+        System.out.println("Sum from 20 to 30 is " + sum);
+
+        sum = 0;
+        for(int i=35;i<=45;i++)
+            sum += i;
+        System.out.println("Sum from 35 to 45 is " + sum);
+    }
+}
