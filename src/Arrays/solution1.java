7fae85542db5a417c3431753875e1d6be6c5402d
subinmun1997 <qzxy812@gmail.com>
2021-11-22 23:38:42 +0900

    solution1

---
+++ src/Arrays/solution1.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class solution1 {
+    public static void main(String[] args) {
+        double[] myList = {1.9, 2.9, 3.4, 3.5};
+        System.out.println(myList[0]);
+
+        double[] myList2 = new double[4];
+        myList2[0] = 1.9;
+        myList2[1] = 2.9;
+        myList2[2] = 3.4;
+        myList2[3] = 3.5;
+        System.out.println(myList2[0]);
+
+
+
+    }
+}
