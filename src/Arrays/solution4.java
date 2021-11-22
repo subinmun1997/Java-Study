6413f93eaa2b43bc40c76296b48f53df0bf3f669
subinmun1997 <qzxy812@gmail.com>
2021-11-23 00:12:17 +0900

    solution4

---
+++ src/Arrays/solution4.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class solution4 {
+    public static void main(String[] args) {
+        double[] myList = {1.1, 2.2, 3.3, 4.4, 5.5};
+        // Enhanced for loop / for-each loop
+        for(double value : myList) {
+            System.out.println(value);
+        }
+    }
+}
