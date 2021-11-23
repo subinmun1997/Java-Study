c05e0a0aed0e54f3c3f0992bec51a6ac664dbe80
subinmun1997 <qzxy812@gmail.com>
2021-11-23 18:38:53 +0900

    solution5

---
+++ src/Object_Classes/solution5.java
@@ -0,0 +1,11 @@
+package Object_Classes;
+
+public class solution5 {
+    public static void main(String[] args) {
+        //java assigns no default value to a local variable inside a method
+        int x; // x has no default value
+        String y; //  y has no default value
+        System.out.println("x is " + x);
+        System.out.println("y is " + y);
+    }
+}
