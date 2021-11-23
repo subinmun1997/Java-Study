3e212aa2f063fcb3b11aeecbdcc00ad29324423d
subinmun1997 <qzxy812@gmail.com>
2021-11-23 17:19:48 +0900

    solution1

---
+++ src/Object_Classes/solution1.java
@@ -0,0 +1,43 @@
+package Object_Classes;
+
+public class solution1 {
+    public static void main(String[] args) {
+        Bicycle bike1 = new Bicycle();
+        Bicycle bike2 = new Bicycle();
+
+        bike1.changeCadence(50);
+        bike1.speedUp(10);
+        bike1.changeGear(2);
+        bike1.printStates();
+
+        bike2.changeCadence(50);
+        bike2.speedUp(10);
+        bike2.changeGear(2);
+        bike2.changeCadence(40);
+        bike2.speedUp(10);
+        bike2.changeGear(3);
+        bike2.printStates();
+    }
+}
+
+class Bicycle {
+    int cadence = 0;
+    int speed = 0;
+    int gear = 1;
+
+    void changeCadence(int newValue) {
+        cadence = newValue;
+    }
+    void changeGear(int newValue) {
+        gear = newValue;
+    }
+    void speedUp(int increment) {
+        speed += increment;
+    }
+    void applyBrakes(int decrement) {
+        speed -= decrement;
+    }
+    void printStates() {
+        System.out.println("cadence: " + cadence + " speed: " + speed + " gear: " + gear);
+    }
+}
