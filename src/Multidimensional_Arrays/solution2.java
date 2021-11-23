d8cbc421cd59fe6fbbecb646bbcfbbb72e2eeec4
subinmun1997 <qzxy812@gmail.com>
2021-11-23 14:24:37 +0900

    solution2

---
+++ src/Multidimensional_Arrays/solution2.java
@@ -0,0 +1,18 @@
+package Multidimensional_Arrays;
+
+public class solution2 {
+    public static void main(String[] args) {
+        int[][] array = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9},
+            {10, 11, 12}
+        };
+        for(int i=0;i<array.length;i++) {
+            for(int j=0;j<array[0].length;j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
