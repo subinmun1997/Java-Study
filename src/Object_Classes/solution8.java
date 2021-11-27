dd5e73775411878d5557edaa6bd1fc1995a133eb
subinmun1997 <qzxy812@gmail.com>
2021-11-27 23:00:47 +0900

    solution8

---
+++ src/Object_Classes/solution8.java
@@ -0,0 +1,9 @@
+package Object_Classes;
+import java.util.Date;
+
+public class solution8 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date.toString());
+    }
+}
