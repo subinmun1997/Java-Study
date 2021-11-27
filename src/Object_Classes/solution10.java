42591899d851e6b722a0b0d93943880a23b68d85
subinmun1997 <qzxy812@gmail.com>
2021-11-28 00:04:35 +0900

    solution10

---
+++ src/Object_Classes/solution10.java
@@ -0,0 +1,13 @@
+package Object_Classes;
+import java.util.ArrayList;
+
+public class solution10 {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Kia");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
