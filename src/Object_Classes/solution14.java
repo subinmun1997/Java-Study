783eb38612efdf127627967f34c10be0f592b050
subinmun1997 <qzxy812@gmail.com>
2021-11-28 00:34:48 +0900

    solution14

---
+++ src/Object_Classes/solution14.java
@@ -0,0 +1,13 @@
+package Object_Classes;
+import java.util.LinkedList;
+
+public class solution14 {
+    public static void main(String[] args) {
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Kia");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
