dd46630f64fc24bdefc5f87e0675e22e428a0a82
subinmun1997 <qzxy812@gmail.com>
2021-11-28 00:52:39 +0900

    solution16

---
+++ src/Object_Classes/solution16.java
@@ -0,0 +1,13 @@
+package Object_Classes;
+import java.util.HashMap;
+
+public class solution16 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> people = new HashMap<String, Integer>();
+        people.put("John", 32);
+        people.put("Steve", 30);
+        people.put("Angie", 33);
+
+        System.out.println(people);
+    }
+}
