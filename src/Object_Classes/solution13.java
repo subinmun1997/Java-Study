fddf9e9e4e59522fc2f7c5da23773743941ada7b
subinmun1997 <qzxy812@gmail.com>
2021-11-28 00:25:38 +0900

    solution13

---
+++ src/Object_Classes/solution13.java
@@ -0,0 +1,16 @@
+package Object_Classes;
+
+import java.util.ArrayList;
+
+public class solution13 {
+    public static void main(String[] args) {
+        ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+        myNumbers.add(10);
+        myNumbers.add(15);
+        myNumbers.add(20);
+        myNumbers.add(25);
+        for(int i : myNumbers) {
+            System.out.println(i);
+        }
+    }
+}
