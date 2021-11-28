37f017172a2ad54af22b6d9bc290cbb2fa070157
subinmun1997 <qzxy812@gmail.com>
2021-11-28 13:56:30 +0900

    solution20

---
+++ src/Object_Classes/solution20.java
@@ -0,0 +1,9 @@
+package Object_Classes;
+
+public class solution20 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
