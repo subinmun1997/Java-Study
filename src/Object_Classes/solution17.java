8f119ba9cfc61e216278d4747aaaee0f480e21af
subinmun1997 <qzxy812@gmail.com>
2021-11-28 13:29:12 +0900

    solution17

---
+++ src/Object_Classes/solution17.java
@@ -0,0 +1,10 @@
+package Object_Classes;
+
+public class solution17 {
+    public static void main(String[] args) {
+        System.out.println("Welcome".replace('e','A'));
+        System.out.println("Welcome".replaceFirst("e","AB"));
+        System.out.println("Welcome".replaceAll("e","AB"));
+        System.out.println("Welcome".replace("el","AB"));
+    }
+}
