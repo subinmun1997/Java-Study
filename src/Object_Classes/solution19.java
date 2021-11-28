13fb77493be545b5666c4073534579146674701a
subinmun1997 <qzxy812@gmail.com>
2021-11-28 13:55:16 +0900

    solution19

---
+++ src/Object_Classes/solution19.java
@@ -0,0 +1,10 @@
+package Object_Classes;
+
+public class solution19 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello ");
+        sb.append("Java");
+
+        System.out.println(sb);
+    }
+}
