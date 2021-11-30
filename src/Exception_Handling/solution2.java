0e5c64c098ea21e1a6984105e18c7313a2e2c177
subinmun1997 <qzxy812@gmail.com>
2021-11-30 21:50:29 +0900

    solution2

---
+++ src/Exception_Handling/solution2.java
@@ -0,0 +1,9 @@
+package Exception_Handling;
+
+public class solution2 {
+    public static void main(String[] args) {
+        int[] myNumbers = {1,2,3};
+        //ArrayIndexOutOfBoundsException
+        System.out.println(myNumbers[10]);
+    }
+}
