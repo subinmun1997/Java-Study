c3e7972d610522e44e8c26f84ba5523b63b31109
subinmun1997 <qzxy812@gmail.com>
2021-11-30 22:02:18 +0900

    solution4

---
+++ src/Exception_Handling/solution4.java
@@ -0,0 +1,12 @@
+package Exception_Handling;
+
+public class solution4 {
+    public static void main(String[] args) {
+        try {
+            int[] myNumbers = {1,2,3};
+            System.out.println(myNumbers[10]);
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
+    }
+}
