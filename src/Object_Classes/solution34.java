af622fec3cf517901405ec1080c2496c00186a40
subinmun1997 <qzxy812@gmail.com>
2021-11-30 18:45:09 +0900

    solution34

---
+++ src/Object_Classes/solution34.java
@@ -0,0 +1,17 @@
+package Object_Classes;
+
+import java.util.HashMap;
+
+public class solution34 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> people = new HashMap<String, Integer>();
+
+        people.put("John", 32);
+        people.put("Steve", 30);
+        people.put("Angie", 33);
+
+        for(String i : people.keySet()) {
+            System.out.println("key: " + i + " value: " + people.get(i));
+        }
+    }
+}
