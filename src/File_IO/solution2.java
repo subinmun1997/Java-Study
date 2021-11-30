17c2827766a598d38be1ada48cda966568081588
subinmun1997 <qzxy812@gmail.com>
2021-11-30 23:23:08 +0900

    solution2

---
+++ src/File_IO/solution2.java
@@ -0,0 +1,18 @@
+package File_IO;
+
+public class solution2 {
+    public static void main(String[] args) throws java.io.IOException {
+        java.io.File file = new java.io.File("scores.txt");
+        if(file.exists()) {
+            System.out.println("File already exists");
+            System.exit(0);
+        }
+        java.io.PrintWriter output = new java.io.PrintWriter(file);
+        output.print("John T Smith ");
+        output.println(90);
+        output.print("Eric K Jones ");
+        output.println(85);
+
+        output.close();
+    }
+}
