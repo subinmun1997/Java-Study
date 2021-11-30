3eb4cdf3aaa82a80d3cb541ff9a024d7b443a957
subinmun1997 <qzxy812@gmail.com>
2021-11-30 17:42:12 +0900

    solution29

---
+++ src/Object_Classes/solution29.java
@@ -0,0 +1,13 @@
+package Object_Classes;
+
+public class solution29 {
+    public static void main(String[] args) {
+
+    }
+}
+abstract class Animal2 {
+    public abstract void animalSound();
+    public void sleep() {
+        System.out.println("Zzz");
+    }
+}
