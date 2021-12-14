d450afad0369eefd5f2ced6c41965c96f8af1660
subinmun1997 <qzxy812@gmail.com>
2021-12-14 23:36:37 +0900

    solution8

---
+++ src/FinalStudy/solution8.java
@@ -0,0 +1,20 @@
+package FinalStudy;
+
+public class solution8 {
+    public static void main(String[] args) {
+        int x = 1;
+        int[] y = new int[10];
+
+        System.out.println("x is " + x);
+        System.out.println("y[0] is " + y[0]);
+
+        m(x, y);
+
+        System.out.println("x is " + x);
+        System.out.println("y[0] is " + y[0]);
+    }
+    public static void m(int number, int[] numbers) {
+        number = 1001;
+        numbers[0] = 5555;
+    }
+}
