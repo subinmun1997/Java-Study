b09c37b6524de91946801812ea69f70d955835e5
subinmun1997 <qzxy812@gmail.com>
2021-12-14 23:20:20 +0900

    solution5

---
+++ src/FinalStudy/solution5.java
@@ -0,0 +1,10 @@
+package FinalStudy;
+
+public class solution5 {
+    public static void main(String[] args) {
+        int[] myList = {1,2,3,4,5};
+        for(int value : myList) {
+            System.out.print(value + " ");
+        }
+    }
+}
