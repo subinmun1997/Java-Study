e3e38633dcce78cc60904e4e4df433d9ccee25d1
subinmun1997 <qzxy812@gmail.com>
2021-12-15 11:46:52 +0900

    solution14

---
+++ src/FinalStudy/solution14.java
@@ -0,0 +1,38 @@
+package FinalStudy;
+
+public class solution14 {
+    public static void main(String[] args) {
+        CircleWithPrivateDataFields myCircle = new CircleWithPrivateDataFields(5.0);
+        System.out.println("The area of the circle of radius " + myCircle.getRadius() + " is " + myCircle.getArea());
+
+        myCircle.setRadius(myCircle.getRadius() * 1.1);
+        System.out.println("The area of the circle of radius " + myCircle.getRadius() + " is " + myCircle.getArea());
+
+        System.out.println("The number of objects created is " + CircleWithPrivateDataFields.getNumberOfObjects());
+    }
+}
+
+class CircleWithPrivateDataFields {
+    private double radius = 1;
+    private static int numberOfObjects = 0;
+
+    public CircleWithPrivateDataFields() {
+        numberOfObjects++;
+    }
+    public CircleWithPrivateDataFields(double newRadius) {
+        radius = newRadius;
+        numberOfObjects++;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double newRadius) {
+        radius = (newRadius >= 0) ? newRadius : 0;
+    }
+    public static int getNumberOfObjects() {
+        return numberOfObjects;
+    }
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+}
