a4395a930ffb6d735f2cdb8548bb54ba7a7c1d09
subinmun1997 <qzxy812@gmail.com>
2021-12-15 12:53:18 +0900

    solution18

---
+++ src/FinalStudy/solution18.java
@@ -0,0 +1,13 @@
+package FinalStudy;
+import java.util.LinkedList;
+
+public class solution18 {
+    public static void main(String[] args) {
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Kia");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
