b32f22954118f49537331aa6f424c1012f494c0a
subinmun1997 <qzxy812@gmail.com>
2021-12-15 13:26:20 +0900

    solution20

---
+++ src/FinalStudy/solution20.java
@@ -0,0 +1,14 @@
+package FinalStudy;
+import java.util.HashMap;
+
+public class solution20 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> people = new HashMap<String, Integer>();
+
+        people.put("John", 32);
+        people.put("Steve", 30);
+        people.put("Angie", 33);
+
+        System.out.println(people);
+    }
+}
