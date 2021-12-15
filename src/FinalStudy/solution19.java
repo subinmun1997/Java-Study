773bd5f18fbb719573442570c4c52d2c2f8fa3ab
subinmun1997 <qzxy812@gmail.com>
2021-12-15 13:09:31 +0900

    solution19

---
+++ src/FinalStudy/solution19.java
@@ -0,0 +1,16 @@
+package FinalStudy;
+import java.util.HashMap;
+
+public class solution19 {
+    public static void main(String[] args) {
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+        System.out.println(capitalCities);
+
+        System.out.println(capitalCities.get("England"));
+        capitalCities.remove("England");
+    }
+}
