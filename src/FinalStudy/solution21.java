c3f76a2c22414a5ac3c14504cc91c12e4b573b45
subinmun1997 <qzxy812@gmail.com>
2021-12-15 13:31:34 +0900

    solution21

---
+++ src/FinalStudy/solution21.java
@@ -0,0 +1,12 @@
+package FinalStudy;
+
+public class solution21 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello ");
+        sb.append("Java");
+        System.out.println(sb);
+
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
