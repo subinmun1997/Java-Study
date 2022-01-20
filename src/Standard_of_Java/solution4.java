a1bb1a86d9c429df00f5a80b4874e80a0fbf78ca
subinmun1997 <qzxy812@gmail.com>
2022-01-20 22:04:58 +0900

    solution4

---
+++ src/Standard_of_Java/solution4.java
@@ -0,0 +1,19 @@
+package Standard_of_Java;
+
+public class solution4 {
+
+    public static void main(String[] args) {
+        String name = "Ja" + "va";
+        String str = name + 8.0;
+
+        System.out.println("name = " + name);
+        System.out.println("str = " + str);
+        System.out.println(7 + " ");
+        System.out.println(" " + 7);
+        System.out.println(7 + "");
+        System.out.println("" + 7);
+        System.out.println("" + "");
+        System.out.println(7 + 7 + "");
+        System.out.println("" + 7 + 7);
+    }
+}
